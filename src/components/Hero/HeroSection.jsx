2068f907a7d0626e15f12b55ac9b397a6b2c3420
rayenbakali <rayen.bakali@gmail.com>
2025-06-16 23:59:13 +0000

    adjusting hero section

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -1,8 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { ArrowDownCircle, ArrowUpRight } from "lucide-react";
+import { ArrowDown,Paintbrush } from "lucide-react";
 import { motion } from "framer-motion";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   const [titleIndex, setTitleIndex] = useState(0);
@@ -16,7 +15,7 @@ const HeroSection = () => {
   return (
     <div id="home" className="h-screen flex items-center justify-center px-6">
       <div className="text-center max-w-2xl">
-        <h1 className="mt-6 text-4xl sm:text-5xl md:text-6xl md:leading-[1.2] font-bold">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl md:leading-[1.2] font-bold">
           Hi im Rayen Bakali
         </h1>
         <h1 className="text-2xl font-bold pt-4">
@@ -35,18 +34,18 @@ const HeroSection = () => {
           and a freelance Graphic Designer
         </p>
         <div className="mt-12 flex items-center justify-center gap-4">
-          <Button size="lg" className="rounded-full text-base">
-            Get to know me <ArrowDownCircle className="!h-5 !w-5" />
+          <Button size="lg" className="rounded-full text-base" onClick={() => window.location.href = "#about"}>
+              Get to know me <ArrowDown className="!h-5 !w-5" />
           </Button>
           <Button 
             variant="outline"
             size="lg"
             className="rounded-full text-base shadow-none"
           >
-            <Link to="/about" className="flex items-center gap-2">
+            <a href="https://www.behance.net/lazerxtv"  target="_blank" className="flex items-center gap-2">
               My other side
-              <ArrowUpRight className="!h-5 !w-5" />
-            </Link>
+              <Paintbrush className="!h-5 !w-5" />
+            </a>
           </Button>
         </div>
       </div>
