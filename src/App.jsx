eedc1e858d3ec648c3504a41406da4106c4a3267
rayenbakali <rayen.bakali@gmail.com>
2025-06-17 00:12:18 +0000

    adding tracing beam, fancy

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import HeroSection from "./components/Hero/HeroSection"
 import Footer from "./components/Footer"
 import WorkTimeline from "./components/WorkTimeline"
 import { Separator } from "./components/ui/separator"
+import { TracingBeam } from "./components/ui/tracing-beam"
 
 function App() {
   return (
     <>
       <Navbar/>
+      <TracingBeam className="mt-12">
       <HeroSection id="home"/>
       <Separator />  
       <WorkTimeline id="about"/>
       <Separator />
       <Footer id="contact"/>
+      </TracingBeam>
     </> 
   );
 }
